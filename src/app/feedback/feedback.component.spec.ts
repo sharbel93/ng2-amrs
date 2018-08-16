0fd611af3732ea90994b7bef83700188c9675372
Sharbel Chris <sharbel.chris@gmail.com>
2018-08-16 14:30:10 +0300

    updating feedback lint issues

--- src/app/feedback/feedback.component.spec.ts
+++ src/app/feedback/feedback.component.spec.ts
@@ -1,9 +1,21 @@
 import { FeedBackHistoryComponent } from './../message-history/messages-history.component';
 
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { TestBed, async, ComponentFixture, inject, fakeAsync } from '@angular/core/testing';
+import {
+  TestBed,
+  async,
+  ComponentFixture,
+  inject,
+  fakeAsync
+} from '@angular/core/testing';
 import { MockBackend, MockConnection } from '@angular/http/testing';
-import { Http, Response, Headers, BaseRequestOptions, ResponseOptions } from '@angular/http';
+import {
+  Http,
+  Response,
+  Headers,
+  BaseRequestOptions,
+  ResponseOptions
+} from '@angular/http';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
@@ -11,137 +23,135 @@ import { FeedBackService } from './feedback.service';
 import { FeedBackComponent } from './feedback.component';
 import { BusyModule, BusyConfig } from 'angular2-busy';
 import { UserService } from '../openmrs-api/user.service';
-import { UserDefaultPropertiesService }
-    from '../user-default-properties/user-default-properties.service';
+// tslint:disable-next-line:max-line-length
+import { UserDefaultPropertiesService } from '../user-default-properties/user-default-properties.service';
 import { VirtualScrollModule } from 'angular2-virtual-scroll';
 import { MomentModule } from 'angular2-moment';
 import { MarkdownModule } from 'angular2-markdown';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { AppSettingsService } from '../app-settings';
 import { LocalStorageService } from './../utils/local-storage.service';
-=======
-import { DepartmentProgramsConfigService
-} from '../etl-api/department-programs-config.service';
->>>>>>> 8c30b620ec556fff7ea269ae8f85ce38d043d0e3
+import { DepartmentProgramsConfigService } from '../etl-api/department-programs-config.service';
+
 class DataStub {
-
-    public postFeedback(payload): Observable<any> {
-        return Observable.of({ status: 'okay' });
-    }
-
+  public postFeedback(payload): Observable<any> {
+    return Observable.of({ status: 'okay' });
+  }
 }
 class UserServiceStub {
-    person = {
-        display: 'test persion'
+  private person = {
+    display: 'test persion'
+  };
+  private getLoggedInUser() {
+    return {
+      person: this.person
     };
-    getLoggedInUser() {
-        return {
-            person: this.person
-        };
-    }
+  }
 }
 class UserDefaultPropertiesServiceStub {
-    getCurrentUserDefaultLocationObject() {
-        return {
-            display: 'Location test'
-        };
-    }
+  public getCurrentUserDefaultLocationObject() {
+    return {
+      display: 'Location test'
+    };
+  }
 }
 
 class FakeDepartmentProgramsConfigService {
-
-  getDartmentProgramsConfig(): Observable<any> {
+  private getDartmentProgramsConfig(): Observable<any> {
     return Observable.of({ status: 'okay' });
   }
-
 }
 
 describe('FeedBackComponent', () => {
-    let fixture: ComponentFixture<FeedBackComponent>;
-    let comp: FeedBackComponent;
-    let dataStub: FeedBackService;
+  let fixture: ComponentFixture<FeedBackComponent>;
+  let comp: FeedBackComponent;
+  let dataStub: FeedBackService;
 
-    beforeEach(async(() => {
-        TestBed.configureTestingModule({
-            imports: [FormsModule, BusyModule, 
-                VirtualScrollModule, MomentModule, MarkdownModule.forRoot()],
-            declarations: [FeedBackComponent, FeedBackHistoryComponent]
-        }).overrideComponent(FeedBackComponent, {
-            set: {
-                providers: [
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        BusyModule,
+        VirtualScrollModule,
+        MomentModule,
+        MarkdownModule.forRoot()
+      ],
+      declarations: [FeedBackComponent, FeedBackHistoryComponent]
+    })
+      .overrideComponent(FeedBackComponent, {
+        set: {
+          providers: [
+            {
+              provide: UserDefaultPropertiesService,
+              useClass: UserDefaultPropertiesServiceStub
+            },
+            {
+              provide: DepartmentProgramsConfigService,
+              useClass: FakeDepartmentProgramsConfigService
+            },
+            { provide: FeedBackService, useClass: DataStub },
+            { provide: UserService, useClass: UserServiceStub },
+            {
+              provide: Http,
+              useFactory: (backend, options) => {
+                return new Http(backend, options);
+              },
+              deps: [MockBackend, BaseRequestOptions]
+            },
+            MockBackend,
+            BaseRequestOptions,
+            AppSettingsService,
+            LocalStorageService
+          ]
+        }
+      })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(FeedBackComponent);
+        comp = fixture.componentInstance;
+        dataStub = fixture.debugElement.injector.get(FeedBackService);
+      });
+  }));
 
-                    {
-                        provide: UserDefaultPropertiesService,
-                        useClass: UserDefaultPropertiesServiceStub
-                    },
-                    {
-                      provide: DepartmentProgramsConfigService,
-                      useClass: FakeDepartmentProgramsConfigService
-                    },
-                      { provide: FeedBackService, useClass: DataStub },
-                    { provide: UserService, useClass: UserServiceStub },
-                    {
-                        provide: Http, useFactory: (backend, options) => {
-                            return new Http(backend, options);
-                        },
-                        deps: [MockBackend, BaseRequestOptions]
-                    },
-                    MockBackend,
-                    BaseRequestOptions, AppSettingsService, LocalStorageService
-                ]
-            }
-        }).compileComponents()
-            .then(() => {
-                fixture = TestBed.createComponent(FeedBackComponent);
-                comp = fixture.componentInstance;
-                dataStub = fixture.debugElement.injector.get(FeedBackService);
-            });
-    }));
+  it('should  render properly', () => {
+    fixture.componentInstance.ngOnInit();
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelectorAll('button').length).toBe(3);
+    expect(fixture.nativeElement.querySelectorAll('input').length).toBe(1);
+    expect(fixture.nativeElement.querySelectorAll('textarea').length).toBe(1);
+  });
 
-    it('should  render properly', () => {
-        fixture.componentInstance.ngOnInit();
-        fixture.detectChanges();
-        expect(fixture.nativeElement
-            .querySelectorAll('button').length).toBe(3);
-        expect(fixture.nativeElement
-            .querySelectorAll('input').length).toBe(1);
-        expect(fixture.nativeElement
-            .querySelectorAll('textarea').length).toBe(1);
-    });
+  it('should hit the success callback when postFeedback returns success', fakeAsync(() => {
+    const spy = spyOn(dataStub, 'postFeedback').and.returnValue(
+      Observable.of({ status: 'okay' })
+    );
+    comp.sendFeedBack();
+    fixture.detectChanges();
+    expect(comp.success).toEqual(true);
+    expect(spy.calls.any()).toEqual(true);
+  }));
 
-    it('should hit the success callback when postFeedback returns success', fakeAsync(() => {
-        const spy = spyOn(dataStub, 'postFeedback').and.returnValue(
-            Observable.of({ status: 'okay' })
-        );
-        comp.sendFeedBack();
-        fixture.detectChanges();
-        expect(comp.success).toEqual(true);
-        expect(spy.calls.any()).toEqual(true);
-    }));
+  it('should hit the error callback when postFeedback returns an error', fakeAsync(() => {
+    const spy = spyOn(dataStub, 'postFeedback').and.returnValue(
+      Observable.throw({ error: '' })
+    );
+    comp.sendFeedBack();
+    fixture.detectChanges();
+    expect(comp.error).toEqual(true);
+    expect(spy.calls.any()).toEqual(true);
+  }));
 
-    it('should hit the error callback when postFeedback returns an error', fakeAsync(() => {
-        const spy = spyOn(dataStub, 'postFeedback').and.returnValue(
-            Observable.throw({ error: '' })
-        );
-        comp.sendFeedBack();
-        fixture.detectChanges();
-        expect(comp.error).toEqual(true);
-        expect(spy.calls.any()).toEqual(true);
-    }));
-
-
-    it('should call back when goBack is called', () => {
-        spyOn(window.history, 'back');
-        comp.goBack();
-        expect(window.history.back).toHaveBeenCalled();
-    });
-    it('should update status when dismiss functions are called', () => {
-        comp.success = true;
-        comp.error = true;
-        comp.dismissError();
-        comp.dismissSuccess();
-        expect(comp.success).toBeFalsy();
-        expect(comp.error).toBeFalsy();
-    });
+  it('should call back when goBack is called', () => {
+    spyOn(window.history, 'back');
+    comp.goBack();
+    expect(window.history.back).toHaveBeenCalled();
+  });
+  it('should update status when dismiss functions are called', () => {
+    comp.success = true;
+    comp.error = true;
+    comp.dismissError();
+    comp.dismissSuccess();
+    expect(comp.success).toBeFalsy();
+    expect(comp.error).toBeFalsy();
+  });
 });
